7a46b6bed037f6bc570a88c6dce50f39f42364d8
AhmedMoEssam1512 <11410120236012@stud.cu.edu.eg>
2026-03-21 16:38:55 +0200

    models fixed

--- backend/models/assigned_model.js
+++ backend/models/assigned_model.js
@@ -1,4 +1,4 @@
-const sequelize = require('../../config/database');
+const sequelize = require('../config/database');
 const { Sequelize, DataTypes } = require('sequelize');
 
 const Assigned = sequelize.define('Assigned', {
--- backend/models/user_model.js
+++ backend/models/user_model.js
@@ -1,4 +1,4 @@
-const sequelize = require('../../config/database');
+const sequelize = require('../config/database');
 const { Sequelize, DataTypes } = require('sequelize');
 
 const User = sequelize.define('User', {
