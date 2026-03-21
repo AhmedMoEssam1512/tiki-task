81cf89d1991d35c3cd80cd8ef931325c5b886f1c
AhmedMoEssam1512 <11410120236012@stud.cu.edu.eg>
2026-03-21 13:06:34 +0200

    frontend setup complete

---
+++ frontend/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>⚽ Tiki Task - Project Management System</h1>
+      <p>Backend is building the API... Frontend will connect soon!</p>
+    </div>
+  );
+}
+
+export default App;
---
+++ frontend/src/pages/Login.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Login() {
+  return (
+    <div>
+      <h1>Login Page - Tiki Task ⚽</h1>
+      <p>Connect this to auth API!</p>
+    </div>
+  );
+}
+
+export default Login;
